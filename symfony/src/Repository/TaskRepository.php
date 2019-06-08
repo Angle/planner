7b38edeedc814446d591367c31662400f260831b
Alejandro Hernandez Guerrero <alexhg11@gmail.com>
2019-06-08 12:18:33 -0500

    TaskRepository : findAllByUserAndWeek

--- symfony/src/Repository/TaskRepository.php
+++ symfony/src/Repository/TaskRepository.php
@@ -4,6 +4,9 @@ namespace App\Repository;
 
 use App\Entity\Notebook;
 use App\Entity\Task;
+use App\Entity\User;
+
+use App\Utility\Week;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
@@ -26,7 +29,7 @@ class TaskRepository extends ServiceEntityRepository
      * @param Notebook $notebook
      * @return Task[] Returns an array of Task objects
      */
-    public function findByNotebook($notebook)
+    public function findByNotebook(Notebook $notebook)
     {
         return $this->createQueryBuilder('t')
             ->andWhere('t.notebook = :notebookId')
@@ -34,5 +37,71 @@ class TaskRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult()
             ;
+    }
+
+    /**
+     * @param User $user
+     * @param Week $week
+     * @return Task[] Returns an array of Task objects
+     */
+    public function findAllByUserAndWeek(User $user, Week $week)
+    {
+        $qb = $this->createQueryBuilder('t');
+
+        // OpenTask Conditions
+        //  (
+        //      (t.openYearNumber = queryYearNumber AND t.openWeekNumber <= queryWeekNumber)
+        //      OR
+        //      (t.openYearNumber < queryYearNumber)
+        //  )
+        // AND
+        // (t.cancelTimestamp IS NULL)
+        // AND
+        // (t.closeTimestamp IS NULL)
+        $conditionOneTaskOrX = $qb->expr()->orX();
+        $conditionOneOpenTaskAndX = $qb->expr()->andX();
+        $conditionOneOpenTaskAndX->add('t.openYearNumber = :queryYearNumber');
+        $conditionOneOpenTaskAndX->add('t.openWeekNumber <= :queryWeekNumber');
+        $conditionOneTaskOrX->add($conditionOneOpenTaskAndX);
+        $conditionOneTaskOrX->add('t.openYearNumber < :queryYearNumber');
+
+        // Open Task Consolidate
+        $globalOpenTaskAndX = $qb->expr()->andX();
+        $globalOpenTaskAndX->add('t.cancelTimestamp IS NULL');
+        $globalOpenTaskAndX->add('t.closeTimestamp IS NULL');
+        $globalOpenTaskAndX->add($conditionOneTaskOrX);
+
+        // Close Task Condition
+        // (
+        //      t.closeYearNumber = queryYearNumber AND t.closeWeekNumber = queryWeekNumber
+        // )
+        $globalCloseTaskAndX = $qb->expr()->andX();
+        $globalCloseTaskAndX->add('t.closeYearNumber = :queryYearNumber');
+        $globalCloseTaskAndX->add('t.closeWeekNumber = :queryWeekNumber');
+
+        // Cancel Task Condition
+        // (
+        //      t.cancelYearNumber = queryYearNumber AND t.canceleWeekNumber = queryWeekNumber
+        // )
+        $globalCancelTaskAndX = $qb->expr()->andX();
+        $globalCancelTaskAndX->add('t.cancelYearNumber = :queryYearNumber');
+        $globalCancelTaskAndX->add('t.cancelWeekNumber = :queryWeekNumber');
+
+        // Consolidate Conditions
+        $globalOrX = $qb->expr()->orX();
+        $globalOrX->add($globalOpenTaskAndX);
+        $globalOrX->add($globalCloseTaskAndX);
+        $globalOrX->add($globalCancelTaskAndX);
+
+        $qb
+            ->innerJoin('r.notebook', 'n')
+            ->where('n.user = :userId')
+            ->andWhere($globalOrX)
+            ->setParameter('userId', $user->getUserId())
+            ->setParameter('queryYearNumber', $week->getYear())
+            ->setParameter('queryWeekNumber', $week->getWeek())
+        ;
+
+        return $qb->getQuery()->getResult();
     }
 }
