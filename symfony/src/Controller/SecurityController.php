84a4dcd9a335399d8d0015c393b71cd5cf91b6d0
Alejandro Hernandez Guerrero <alexhg11@gmail.com>
2019-06-10 12:09:47 -0500

    Registration form buildering...

--- symfony/src/Controller/SecurityController.php
+++ symfony/src/Controller/SecurityController.php
@@ -2,9 +2,20 @@
 
 namespace App\Controller;
 
+use App\Preset\StatusCode;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
+use App\Entity\User;
+use App\Form\UserRegistrationType;
+
+use App\Security\LoginFormAuthenticator;
 
 class SecurityController extends AbstractController
 {
@@ -22,8 +33,49 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    public function register(): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder,
+                             LoginFormAuthenticator $authenticator, GuardAuthenticatorHandler $guardHandler): Response
     {
-        return $this->render('security/register.html.twig');
+        /** @var EntityManagerInterface $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $user = new User();
+
+        $form = $this->createForm(UserRegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // TODO: Validate against re-type password
+            $plainTextPassword = $form->get('checkPassword')->getData();
+
+            $user->setPassword($passwordEncoder->encodePassword(
+                $user,
+                $user->getPassword()
+            ));
+
+            $em->persist($user);
+
+            try {
+                $em->flush();
+            }catch (\Exception $e) {
+                $this->addFlash(StatusCode::LABEL_ERROR, StatusCode::ERROR_DATABASE_INSERT);
+            }
+
+            // after validating the user and saving them to the database
+            // authenticate the user and use onAuthenticationSuccess on the authenticator
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,          // the User object you just created
+                $request,
+                $authenticator, // authenticator whose onAuthenticationSuccess you want to use
+                'main'          // the name of your firewall in security.yaml
+            );
+        }
+
+
+        return $this->render('security/register.html.twig', [
+            'form'  => $form->createView()
+        ]);
     }
 }
